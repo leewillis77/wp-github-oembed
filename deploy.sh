e44229ce7904ef7cc0cbc477c75369f97e440c40
Lee Willis <lee@ademti-software.co.uk>
2020-02-05 10:14:24 +0000

    Deploy fix

--- deploy.sh
+++ deploy.sh
@@ -37,6 +37,7 @@ echo "Versions match in readme.txt and $MAINFILE. Let's proceed..."
 if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION1"
 	then 
 		echo "Version $NEWVERSION1 already exists as git tag."; 
+		COMMITMSG="Committing $NEWVERSION1"
 	else
 		echo "Git version does not exist. Let's proceed..."
 		cd $GITPATH
