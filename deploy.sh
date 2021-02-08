0963e00729fe74607a28100a02b888a5341eed38
Lee Willis <lee@ademti-software.co.uk>
2021-02-08 09:31:50 +0000

    Exclude image from deploy to wp.org for future releases

--- deploy.sh
+++ deploy.sh
@@ -64,7 +64,7 @@ echo "Ignoring github specific files and deployment script"
 svn propset svn:ignore "deploy.sh
 README.md
 .git
-img/cpw.png
+img/cpw2.png
 .gitignore" "$SVNPATH/trunk/"
 
 echo "Changing directory to SVN and committing to trunk"
